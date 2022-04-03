b6738bb739ca583b58b0ef5b6787629f996c6e9d
RedCat995 <100766235+RedCat995@users.noreply.github.com>
2022-04-03 14:48:11 +0200

    Update ChartingState.hx

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -429,6 +429,34 @@ class ChartingState extends MusicBeatState
 			}
 		}
 		super.create();
+		#if android
+	addVirtualPad(FULL, A_B);
+        #end
+
+	//if you want it to have a camera
+        #if android
+	addPadCamera();
+        #end
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+        #if android
+	addAndroidControls();
+        #end
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the code is
+	#if android
+	androidc.visible = false;
+	#end
 	}
 
 	public var texts:FlxTypedGroup<FlxText>;
