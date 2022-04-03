3176aa7abfefa9e45b2beee4f08c3655b9caf16c
RedCat995 <100766235+RedCat995@users.noreply.github.com>
2022-04-03 22:52:53 +0200

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -62,8 +62,8 @@ class TitleState extends MusicBeatState
 		#end*/
 		
 		#if sys
-		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
-			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
+		if (!sys.FileSystem.exists(SUtil.getPath() + "assets/replays"))
+			sys.FileSystem.createDirectory(SUtil.getPath() + "assets/replays");
 		#end
 
 		@:privateAccess
